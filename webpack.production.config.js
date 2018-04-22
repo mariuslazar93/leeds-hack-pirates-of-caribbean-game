6d7af0c1c857b0f48c299105a0bbc0464f8de999
Marius Lazar <mariusl@packtpub.com>
2018-04-22 03:43:36 +0100

    file-loader

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -51,6 +51,12 @@ module.exports = {
         include: path.join(__dirname, 'src'),
       },
       {
+        test: /\.(png|svg|jpg|gif|woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+      {
         test: [/\.vert$/, /\.frag$/],
         use: 'raw-loader'
       }

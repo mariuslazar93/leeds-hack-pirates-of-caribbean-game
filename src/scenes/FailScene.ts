a1bd41b00593d56e546e077fd51675ec2eb27a3f
Marius Lazar <mariusl@packtpub.com>
2018-04-22 11:54:59 +0100

    updated text

--- src/scenes/FailScene.ts
+++ src/scenes/FailScene.ts
@@ -6,7 +6,7 @@ function preload() {
 
 function create() {
   this.banner = this.add.image(400, 300, 'fail-banner');
-  this.failText = this.add.text(config.WIDTH / 2 - 50, config.HEIGHT/2, 'You dissapoint me!\nPress anywhere to restart...', { fontSize: '30px', fill: '#f00', backgroundColor: '#fff' });
+  this.failText = this.add.text(config.WIDTH / 2 - 50, config.HEIGHT/2, 'You disappoint me!\nPress anywhere to restart...', { fontSize: '30px', fill: '#f00', backgroundColor: '#fff' });
   this.input.once('pointerdown', function () {
     this.scene.stop('failScene');
     this.scene.launch('pirateScene');
